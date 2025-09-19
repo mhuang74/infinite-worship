1f70dbfdcd042ecbec0d6ba78bb72aceec1b8f97
Michael S. Huang <mhuang74@gmail.com>
2025-09-20 00:20:14 +0800

    fix script bug

--- application/build.sh
+++ application/build.sh
@@ -5,16 +5,12 @@ TAG=$(date +%Y%m%d)
 
 # Set the image name with the tag
 export IMAGE_NAME="mhuang74/infinite-worship-arm64:$TAG"
-export DOCKER_IMAGE="infinite-worship"
-
 echo "Building image: $IMAGE_NAME for linux/arm64"
 
 # Build the docker image using docker-compose
 # The --pull argument ensures we get the latest base images
 DOCKER_DEFAULT_PLATFORM=linux/arm64 docker-compose build --pull app
 
-echo "Tagging image..."
-docker tag "${DOCKER_IMAGE}_app:latest" "$IMAGE_NAME"
 
 echo "Pushing image to Docker Hub..."
 docker push "$IMAGE_NAME"
