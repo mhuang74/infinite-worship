5a0281b75c3785d1cdea25c4fa8796e1a7399d0a
Michael S. Huang <mhuang74@gmail.com>
2025-09-22 16:09:48 +0800

    push docker image to AWS ECR

--- application/build.sh
+++ application/build.sh
@@ -11,8 +11,17 @@ echo "Building image: $IMAGE_NAME for linux/arm64"
 # The --pull argument ensures we get the latest base images
 DOCKER_DEFAULT_PLATFORM=linux/arm64 docker-compose build --pull app
 
+ECR_REPO="public.ecr.aws/u4p9h6o7/mhuang74/infinite-worship"
 
-echo "Pushing image to Docker Hub..."
-docker push "$IMAGE_NAME"
+echo "Tagging image for AWS ECR..."
+docker tag "$IMAGE_NAME" "$ECR_REPO:$TAG"
+docker tag "$IMAGE_NAME" "$ECR_REPO:latest"
+
+echo "Log into AWS ECR..."
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/u4p9h6o7
+
+#echo "Pushing image to AWS ECR..."
+docker push "$ECR_REPO:$TAG"
+docker push "$ECR_REPO:latest"
 
 echo "Build and push complete."
